5b2eeb1a737551af811ffba3e5938a95f276a8f7
techgaun <coolsamar207@gmail.com>
2019-01-16 21:24:36 -0600

    finish implementing functionality

--- src/commands/pgcli.ts
+++ src/commands/pgcli.ts
@@ -1,4 +1,5 @@
-import {Command, flags} from '@heroku-cli/command'
+import {Command, flags, vars} from '@heroku-cli/command'
+import * as cp from 'child_process'
 
 export default class PGCLI extends Command {
   static description = 'Connect with Heroku posgtresql db using pgcli'
@@ -8,7 +9,7 @@ export default class PGCLI extends Command {
   ]
 
   static args = [
-    {name: 'DATABASE_URL', optional: true, description: 'the Heroku postgres DB name to use. Default: DATABASE_URL'}
+    {name: 'name', optional: true, description: 'the Heroku postgres DB name to use. Default: DATABASE_URL'}
   ]
 
   static flags = {
@@ -16,12 +17,30 @@ export default class PGCLI extends Command {
     app: flags.string({char: 'a', env: 'HEROKU_APP', required: true, description: 'the Heroku app to use'})
   }
 
+  spawn(args: string[]) {
+    return new Promise((resolve, reject) => {
+      let s = cp.spawn('pgcli', args, {stdio: [0, 1, 2]})
+      s.on('error', (err: Error & {code?: string}) => {
+        if (err.code === 'ENOENT') {
+          this.error('pgcli must be installed.')
+        } else reject(err)
+      })
+
+      s.on('close', resolve)
+    })
+  }
+
   async run() {
     const {args, flags} = this.parse(PGCLI)
     const name = args.name || 'DATABASE_URL'
-    const {body: app} = await this.heroku.get(`/apps/${flags.app}`)
+    const {body: configs} = await this.heroku.get(`/apps/${flags.app}/config-vars`)
+    const value = configs[name]
 
-    this.log(app)
+    if (value == null) {
+      this.error(`Config var for ${name} not found`)
+    }
+
     this.log(`Running pgcli on ${name}...`)
+    await this.spawn([value])
   }
 }
